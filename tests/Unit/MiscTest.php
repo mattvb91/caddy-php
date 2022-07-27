87f1e3dea06e14e2963ff71dbf8b78ea9fb1af35
Matthias von Bargen <matt@mavon.ie>
2022-07-27 16:07:38 +0200

    fix finding hosts

---
+++ tests/Unit/MiscTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use mattvb91\CaddyPhp\Config\Apps\Http;
+use PHPUnit\Framework\TestCase;
+use function mattvb91\CaddyPhp\findHost;
+
+class MiscTest extends TestCase
+{
+    /**
+     * @covers \mattvb91\CaddyPhp\findHost
+     */
+    public function test_finding_host()
+    {
+        $http = new Http();
+        $server = new Http\Server();
+        $server->setListen([":80"]);
+
+
+        $host = (new Http\Server\Routes\Match\Host('shops'))
+            ->setHosts([
+                "{http.request.host}.localhost",
+            ]);
+
+        $shopDeploymentsRoute = new Http\Server\Route();
+        $shopDeploymentsRoute->addMatch($host)
+            ->setTerminal(true);
+
+
+        $server->addRoute($shopDeploymentsRoute);
+        $http->addServer('platform', $server);
+
+        $this->assertEquals($host, findHost($http, 'shops')['host']);
+    }
+
+}

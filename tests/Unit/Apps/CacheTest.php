8d368179106245ac1484f704092c0a93ac000e09
Matthias von Bargen <matt@mavon.ie>
2022-09-20 15:17:07 +0200

    add redis option (#11)
    * add redis option

--- tests/Unit/Apps/CacheTest.php
+++ tests/Unit/Apps/CacheTest.php
@@ -27,9 +27,68 @@ class CacheTest extends TestCase
                 'strategy' => 'hard',
             ],
             'log_level' => 'INFO',
+            'stale'     => '3600s',
+            'ttl'       => '3600s',
+        ], $cache->ToArray());
+    }
+
+    /**
+     * @covers \mattvb91\CaddyPhp\Config\Apps\Cache::setNuts
+     * @covers \mattvb91\CaddyPhp\Config\Apps\Cache::toArray
+     */
+    public function testSetNutsCache()
+    {
+        $cache = new Cache();
+        $nuts = new Cache\Nuts();
+
+        $cache->setNuts($nuts);
+        $this->assertEquals([
+            'api'       => [
+                'basepath' => '/cache',
+                'souin'    => [
+                    'basepath' => '/souin',
+                    'enable'   => true,
+                ],
+            ],
+            'cdn'       => [
+                'dynamic'  => true,
+                'strategy' => 'hard',
+            ],
             'nuts'      => [
                 'path' => '/tmp/nuts-souin',
             ],
+            'log_level' => 'INFO',
+            'stale'     => '3600s',
+            'ttl'       => '3600s',
+        ], $cache->ToArray());
+    }
+
+    /**
+     * @covers \mattvb91\CaddyPhp\Config\Apps\Cache::setRedis
+     * @covers \mattvb91\CaddyPhp\Config\Apps\Cache::toArray
+     */
+    public function testSetRedisCache()
+    {
+        $cache = new Cache();
+        $redis = new Cache\Redis('localhost:6379');
+
+        $cache->setRedis($redis);
+        $this->assertEquals([
+            'api'       => [
+                'basepath' => '/cache',
+                'souin'    => [
+                    'basepath' => '/souin',
+                    'enable'   => true,
+                ],
+            ],
+            'cdn'       => [
+                'dynamic'  => true,
+                'strategy' => 'hard',
+            ],
+            'redis'     => [
+                'url' => 'localhost:6379',
+            ],
+            'log_level' => 'INFO',
             'stale'     => '3600s',
             'ttl'       => '3600s',
         ], $cache->ToArray());

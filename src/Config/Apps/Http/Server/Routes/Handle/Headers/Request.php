91fe02196175590a2280b1214ba9cd9bc7ac7ac0
Matthias von Bargen <matt@mavon.ie>
2023-11-11 20:19:40 +0100

    request add header (#16)

--- src/Config/Apps/Http/Server/Routes/Handle/Headers/Request.php
+++ src/Config/Apps/Http/Server/Routes/Handle/Headers/Request.php
@@ -15,6 +15,24 @@ class Request implements Arrayable
      */
     private array $delete = [];
 
+    /**
+     * @var array<string, string[]>
+     */
+    private array $add = [];
+
+
+    /**
+     * @param string $name
+     * @param string[] $values
+     * @return $this
+     */
+    public function addHeader(string $name, array $values): static
+    {
+        $this->add[$name] = $values;
+
+        return $this;
+    }
+
     public function addDeleteHeader(string $header): static
     {
         $this->delete[] = $header;
@@ -28,6 +46,10 @@ class Request implements Arrayable
 
         if (count($this->delete)) {
             $array['delete'] = $this->delete;
+        }
+
+        if (count($this->add)) {
+            $array['add'] = $this->add;
         }
 
         return $array;
